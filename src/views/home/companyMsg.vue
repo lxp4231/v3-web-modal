3e4148f8a4a09e608db785e830493344130f9b74
liuxiangpeng <1570807213@qq.com>
2024-02-21 14:30:10 +0800

    更新

--- src/views/home/companyMsg.vue
+++ src/views/home/companyMsg.vue
@@ -1,89 +1,24 @@
 <template>
-  <div>
-    <el-table
-      border
-      stripe
-      :data="tableData"
-      style="width: 100%; margin-bottom: 20px"
-      row-key="id"
-      default-expand-all
-      :header-cell-style="handerMethod"
-    >
-      <el-table-column prop="date" label="组名" />
-      <el-table-column prop="name" label="" />
-      <el-table-column prop="address" label="" />
-      <el-table-column label="" width="100" fixed="right">
-        <template #default="scope">
-          <el-button v-if="scope.row?.type" size="small" type="primary" link @click="onEdit(scope.row)">编辑</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+  <div id="container"></div>
 </template>
 
 <script setup lang="ts">
-const tableData: any[] = [
-  {
-    id: 3,
-    date: '四港',
-    isGroup: true,
-    children: [
-      {
-        id: 31,
-        name: 'wangxiaohu',
-        isGroup: false,
-        address: 'No. 189, Grove St, Los Angeles',
-      },
-      {
-        id: 32,
-        date: '云平台',
-        isGroup: true,
-        children: [
-          {
-            id: 332,
-            date: '物流宝',
-            name: 'wangxiaohu',
-            address: 'No. 189, Grove St, Los Angeles',
-            type: true,
-            isGroup: false,
-          },
-          {
-            id: 213,
-            date: '纾困',
-            name: 'wangxiaohu',
-            address: 'No. 189, Grove St, Los Angeles',
-            isGroup: false,
-          },
-        ],
-      },
-    ],
-  },
-];
-const onEdit = (row: any) => {
-  console.log(row, 'row');
-};
-// eslint-disable-next-line consistent-return
-const handerMethod = (row: any) => {
-  // 第一级表头
-  if (row.row[0].level === 1) {
-    // 合并第一三列
-    // eslint-disable-next-line no-param-reassign
-    row.row[0].colSpan = 4;
-    // row.row[1].colSpan = 0;
-    // 设置第一列表头的样式
-    if (row.columnIndex === 0) {
-      return {
-        background: '#F5F7FA',
-        color: '#18233F',
-        fontSize: 'bold',
-        fontFamily: 'PingFangSC, PingFang SC',
-      };
-    }
-  } else {
-    // 其他级别表头样式
-    return { backgroundColor: '#143f6a', color: '#fff' };
-  }
-};
+const map = new BMapGL.Map('container');
+const point = new BMapGL.Point(116.404, 39.915);
+map.centerAndZoom(point, 15);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+#container {
+  width: 500px;
+  height: 500px;
+}
+</style>
