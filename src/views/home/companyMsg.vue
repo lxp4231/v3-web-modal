588e5a0584f2b40ea62f4aef9617f182df424062
liuxiangpeng <1570807213@qq.com>
2024-03-14 16:32:07 +0800

    更新

--- src/views/home/companyMsg.vue
+++ src/views/home/companyMsg.vue
@@ -1,24 +1,60 @@
 <template>
-  <div id="container"></div>
+  <div class="map-wrapper">
+    <div id="shipMap" class="map"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-const map = new BMapGL.Map('container');
-const point = new BMapGL.Point(116.404, 39.915);
-map.centerAndZoom(point, 15);
+import { ref } from 'vue';
+import dayjs from 'dayjs';
+import mockShipPoints from '@/utils/mockShipPoints.json';
+import mockCarPoints from '@/utils/mockCarPoints.json';
+
+const ak = '74a1eacae3214fc4b947eb6974c43f5f';
+const map = ref<any>('');
+const initMapJobId = setInterval(() => {
+  if (window.ShipxyAPI) {
+    const options = {
+      ak,
+      centerPoint: [32.1, 122.11],
+      // 初始缩放级别
+      zoom: 4,
+      // 最小缩放级别
+      minZoom: 2,
+      // 最大缩放级别
+      maxZoom: 18,
+      cjhdTileLayer: { isShow: true },
+      miniMapControl: { isShow: true, options: {} }, // 鹰眼控件
+    };
+    map.value = new ShipxyAPI.Map('shipMap', options);
+    ShipxyAPI.TrackService(map.value);
+    clearInterval(initMapJobId);
+    // 展示轨迹
+    // this.showShipTrack();
+  }
+}, 1000);
 </script>
 
 <style lang="scss" scoped>
-html {
+.map-wrapper {
+  position: relative;
+  width: 100%;
   height: 100%;
-}
-body {
-  height: 100%;
-  margin: 0px;
-  padding: 0px;
-}
-#container {
-  width: 500px;
-  height: 500px;
+  background: #d9d9d9;
+  border-radius: 0px 0px 0px 0px;
+  opacity: 1;
+  border-radius: 8px;
+
+  .map {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    outline: none;
+    background-color: #a3ccff;
+  }
 }
 </style>
