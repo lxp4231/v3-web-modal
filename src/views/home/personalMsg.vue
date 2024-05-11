ccbacaa13d54e6c14d9d56b6b26ca256ddf244d5
liuxiangpeng <1570807213@qq.com>
2024-05-11 13:42:01 +0800

    更新

--- src/views/home/personalMsg.vue
+++ src/views/home/personalMsg.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import useScreenWidth from '@/use/useScreenWidth';
-
-const { screenWidth } = useScreenWidth();
-const form = ref<any>({
-  containerNo1: '',
-  containerNo2: '1823919281@qq.com',
-  containerNo3: '',
-  containerNo4: '',
+import { GaodeMap } from '@antv/l7-maps';
+import { PolygonLayer } from '@antv/l7';
+import { Scene } from '@antv/l7';
+import data from './data.json'
+// 同样你也可以初始化一个 Mapbox 地图
+const scene = new Scene({
+  id: 'map',
+  map: new GaodeMap({
+    pitch: 35.210526315789465,
+    style: 'dark',
+    center: [104.288144, 31.239692],
+    zoom: 4.4,
+  }),
 });
-const isBtn = ref<boolean>(true);
-const isLoading = ref<boolean>(false);
-
-const onBtn = () => {
-  isBtn.value = !isBtn.value;
-};
+const chinaPolygonLayer = new PolygonLayer({})
+  .source(data)
+  .color('name', [
+    'rgb(239,243,255)',
+    'rgb(189,215,231)',
+    'rgb(107,174,214)',
+    'rgb(49,130,189)',
+    'rgb(8,81,156)',
+  ]);
+  scene.addLayer(chinaPolygonLayer);
 </script>
 <template>
-  <div class="">
-    12
-  </div>
+ <div style="min-height:100%; justify-content: center;position: relative" id="map" />
 </template>
 
 <style lang="scss" scoped>
